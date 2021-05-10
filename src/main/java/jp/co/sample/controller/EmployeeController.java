8603e3896f0ea3cba674aa26f7c1a31b45de29e4
funabakazuki <kazuki.funaba@rakus.partners.co.jp>
2021-05-10 14:07:44 +0900

    従業員⼀覧を表示する機能の作成
    EmployeeService(service) created
    
    UpdateEmployeeForm(form) created
    
    EmployeeController(controller) created
    
    list.html  created
    
    list.html  fixed

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員情報の表示や更新処理を行うクラス
+ * @author funaba
+ *
+ */
+@Controller
+@RequestMapping ("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
+	
+	
+	/**
+	 * 従業員一覧を検索し、「employee/list.html」にフォワードするメソッド
+	 * @param model
+	 * @return employee/list
+	 */
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		model.addAttribute("employeeList", employeeService.showList());
+		
+		return "employee/list";
+	}
+
+}
